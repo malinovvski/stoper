c4cb2ef758926f80ed3b8df255c41780b8ff9680
malinovvski <100025958+malinovvski@users.noreply.github.com>
2022-11-04 17:40:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 
 function start(){
      interval = setInterval(() => {
-        time += 1;
-        stoperElement.innerHTML= time 
+        time += 100;
+        let minutes = Math.floor((time / (60 * 1000)) % 60);
+        let seconds = Math.floor((time / 1000) % 60);
+        let miliseconds = time % 1000 / 10;
+        
+        stoperElement.innerHTML=`${minutes}:${seconds}:${miliseconds}`;
+         
+        
 }, 1000);
     
 
@@ -18,8 +24,12 @@ function stop(){
     stoperElement.innerHTML = time;
     clearInterval(interval);
 };
-let time = 0; 
-let interval = null;
+
+let time = 0;
+let seconds = 0;
+let minutes = 0;
+let miliseconds = 0;
+let interval = 0;
 let stoperElement = document.querySelector("#meter");
 stoperElement.innerHTML = time;
 
