0e344e4572c9a46eed8a08ec7e74e248f03c8125
malinovvski <m.malinovvski@gmail.com>
2022-11-04 18:05:15 +0100

    Stopwatch alpha

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-
 function start(){
      interval = setInterval(() => {
-        time += 1;
-        stoperElement.innerHTML= time 
-}, 1000);
+        time+=1;
+        let minutes = Math.floor((time / (60 * 100)) % 60);
+        let seconds = Math.floor((time / 100) % 60);
+        let miliseconds = time % 100 ;
+         stoperElement.innerHTML=`${minutes}:${seconds}:${miliseconds}`;
+              
+}, 10);
     
-
 };
 
 function pause(){
@@ -17,9 +19,14 @@ function stop(){
     time = 0;
     stoperElement.innerHTML = time;
     clearInterval(interval);
+    stoperElement.innerHTML=`${minutes}:${seconds}:${miliseconds}`;
 };
-let time = 0; 
-let interval = null;
+
+let time = 0;
+let seconds = 0;
+let minutes = 0;
+let miliseconds = 0;
+let interval = 0;
 let stoperElement = document.querySelector("#meter");
 stoperElement.innerHTML = time;
 
