53955848f136aaf6720aae75ee156e0920d27efd
malinovvski <m.malinovvski@gmail.com>
2022-11-07 20:23:08 +0100

    Stopwatch

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-function start(){
-     interval = setInterval(() => {
-        time+=1;
-        let minutes = Math.floor((time / (60 * 100)) % 60);
-        let seconds = Math.floor((time / 100) % 60);
-        let miliseconds = time % 100 ;
-         stoperElement.innerHTML=`${minutes}:${seconds}:${miliseconds}`;
-         
-        
-}, 10);
-    
+
+function start() {
+    interval = setInterval(() => {
+        time += 1;
+        minutes = Math.floor((time / (6000)) % 60);
+        seconds = Math.floor((time / 100) % 60);
+        miliseconds = time % 100;
+        stoperElement.innerHTML = `${minutes}:${seconds}:${miliseconds}`;
+
+    }, 10);
+
 
 };
 
-function pause(){
-    clearInterval(interval);     
-};
-
-
-function stop(){
-    time = 0;
-    stoperElement.innerHTML = time;
+function pause() {
     clearInterval(interval);
-    stoperElement.innerHTML=`${minutes}:${seconds}:${miliseconds}`;
+};
+
+
+function stop() {
+    time = 0;
+    seconds = 0;
+    minutes = 0;
+    miliseconds = 0;
+    clearInterval(interval);
+    stoperElement.innerHTML = `${minutes}:${seconds}:${miliseconds}`;
 };
 
 let time = 0;
@@ -30,7 +32,7 @@ let minutes = 0;
 let miliseconds = 0;
 let interval = 0;
 let stoperElement = document.querySelector("#meter");
-stoperElement.innerHTML = time;
+stoperElement.innerHTML = `${minutes}:${seconds}:${miliseconds}`;
 
 
 
